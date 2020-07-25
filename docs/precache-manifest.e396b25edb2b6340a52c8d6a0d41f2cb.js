d63aed88d94d18166e31e1eda3aa0944cc940f3d
Alexandra <sandra.larrosa88@gmail.com>
2020-07-25 19:20:35 +0200

    Add docs

---
+++ docs/precache-manifest.e396b25edb2b6340a52c8d6a0d41f2cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d64ce7380aab705119753839d07c60bf",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f14190d1449978426b42",
+    "url": "./static/css/main.d25ddfe0.chunk.css"
+  },
+  {
+    "revision": "b8978300590df2f63229",
+    "url": "./static/js/2.be550a61.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.be550a61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f14190d1449978426b42",
+    "url": "./static/js/main.c8611800.chunk.js"
+  },
+  {
+    "revision": "b388ac9c340cc6e2817c",
+    "url": "./static/js/runtime-main.fa365cae.js"
+  }
+]);
